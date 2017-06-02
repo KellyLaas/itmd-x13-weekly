f56c82467335468957de693601e3235742cedf79
KellyLaas <laas@iit.edu>
2017-06-02 19:24:15 -0400

    Add username to flash notice

--- task-b/depot/db/schema.rb
+++ task-b/depot/db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170601124334) do
+ActiveRecord::Schema.define(version: 20170602231132) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.string "image_url"
     t.decimal "price", precision: 8, scale: 2
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
