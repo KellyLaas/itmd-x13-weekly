30a69028acb8b37168dbf3a4c83b9ba6ef570907
KellyLaas <laas@iit.edu>
2017-06-02 21:23:36 -0500

    Edit config.routes.rb

--- task-b/depot/config/routes.rb
+++ task-b/depot/config/routes.rb
@@ -1,13 +1,22 @@
 Rails.application.routes.draw do
-  get 'admin/index'
+  get 'admin' => 'admin#index'
 
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  get 'sessions/destroy'
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
 
   resources :users
-  resources :products
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :orders
+  resources :line_items
+  resources :carts
+  root 'store#index', as: 'store_index'
+
+  resources :products do
+    get :who_bought, on: :member
+end
+
+  # For details on the DSL available within this file, see
+  # http://guides.rubyonrails.org/routing.html
 end
