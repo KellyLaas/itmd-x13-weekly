12e1e74d75bb40860aa50a967c291bdf952fd426
KellyLaas <laas@iit.edu>
2017-06-02 20:10:16 -0500

    Create session in 'pp/controllers/sessions_controller.rb'

--- task-b/depot/config/routes.rb
+++ task-b/depot/config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'admin/index'
+
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   resources :users
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
