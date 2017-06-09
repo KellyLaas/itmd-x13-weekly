725c6276ff38ee692b4ba96384d5e8f45e7363f6
KellyLaas <laas@iit.edu>
2017-06-09 15:16:58 -0500

    Run migration for subject model

--- ethicscases/db/schema.rb
+++ ethicscases/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170609201129) do
+ActiveRecord::Schema.define(version: 20170609201543) do
 
   create_table "authors", force: :cascade do |t|
     t.string "first_name"
@@ -25,6 +25,14 @@ ActiveRecord::Schema.define(version: 20170609201129) do
     t.string "abstract"
     t.string "subject"
     t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "subjects", force: :cascade do |t|
+    t.string "subject"
+    t.string "title"
+    t.string "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
