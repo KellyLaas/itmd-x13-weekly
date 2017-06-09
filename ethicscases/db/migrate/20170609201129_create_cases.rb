54d484df1bef853f26a256523b6eead6ce37d19e
KellyLaas <laas@iit.edu>
2017-06-09 15:11:58 -0500

    Run model for case
      $ rails generate model Case author:string year:string abstract:string
      subject:string subject:string title:string

---
+++ ethicscases/db/migrate/20170609201129_create_cases.rb
@@ -0,0 +1,14 @@
+class CreateCases < ActiveRecord::Migration[5.1]
+  def change
+    create_table :cases do |t|
+      t.string :author
+      t.string :year
+      t.string :abstract
+      t.string :subject
+      t.string :subject
+      t.string :title
+
+      t.timestamps
+    end
+  end
+end
