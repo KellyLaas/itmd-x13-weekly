4fa09bb53cb5cdc72e22498736b1fe3d677a0dd3
KellyLaas <laas@iit.edu>
2017-06-11 15:27:06 -0500

    Add validation to author and case

--- ethicscases/app/models/author.rb
+++ ethicscases/app/models/author.rb
@@ -1,4 +1,11 @@
 class Author < ApplicationRecord
+  has_and_belongs_to_many :cases
+end
+
+class Author < ApplicationRecord
+  has_and_belongs_to_many :subjects
+end
+
   validates :last_name, presence: true,
                         length: { minimum: 2 }
 
--- ethicscases/app/models/case.rb
+++ ethicscases/app/models/case.rb
@@ -1,2 +1,17 @@
 class Case < ApplicationRecord
+has_and_belongs_to_many :subjects
+end
+
+class Case < ApplicationRecord
+has_and_belongs_to_many :authors
+end
+
+validates :title, presence: true,
+                      length: { minimum: 2 }
+          :author, presence: true,
+                       length: { minimum: 2 }
+          :year, presence: true,
+                        length: { minimum: 4 }
+          :abstract, presence: true,
+                        length: { minimum: 2 }
 end
