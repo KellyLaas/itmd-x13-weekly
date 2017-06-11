794e5dc4bfaa48176f122df075fe238ceea1ce35
KellyLaas <laas@iit.edu>
2017-06-11 15:10:22 -0500

    Add valitation to case.rb

--- ethicscases/app/controllers/application_controller.rb
+++ ethicscases/app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 end
+
+  def index
+    @cases = Case.all
+  end
+
+  def show
+    @cases = Case.find (params[:id])
+  end
+
+  def new
+  end 
--- ethicscases/app/models/author.rb
+++ ethicscases/app/models/author.rb
@@ -1,2 +1,7 @@
 class Author < ApplicationRecord
+  validates :last_name, presence: true,
+                        length: { minimum: 2 }
+
+            :first_name, presence: true,
+                         length: { minimum: 2 }
 end
