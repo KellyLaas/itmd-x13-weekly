f34381849e7c80c0472b04696de217189898a57f
KellyLaas <laas@iit.edu>
2017-06-11 10:18:23 -0500

    Create show.html.haml

--- ethicscases/app/controllers/cases_controller.rb
+++ ethicscases/app/controllers/cases_controller.rb
@@ -1,2 +1,19 @@
 class CasesController < ApplicationController
+  def show
+    @case = Case.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @case = Case.new(case_params)
+
+    @case.save
+    redirect_to @case
 end
+
+private
+  def.case_params
+    params.require(:case).permit(:title .:abstract, :year, :subject, :author)
+  end
