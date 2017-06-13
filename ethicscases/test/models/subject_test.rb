dc6502b348e05506f5ffe7c4ea5a950099b74095
KellyLaas <laas@iit.edu>
2017-06-13 12:02:22 -0500

    Change test  model for subject to require content

--- ethicscases/test/models/subject_test.rb
+++ ethicscases/test/models/subject_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
-class SubjectTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  class SubjectTest < ActiveSupport::TestCase
+    test "should not save subject without content" do
+      subject = Subject.new
+      assert_not subject.save
+    end
 end
