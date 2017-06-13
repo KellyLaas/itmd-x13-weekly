f29b99e7f6bdf76c87dfd2bfb8ac298d2e63a0aa
KellyLaas <laas@iit.edu>
2017-06-13 18:31:31 -0500

    Add validation for year and abstract - correctly

--- ethicscases/test/models/case_test.rb
+++ ethicscases/test/models/case_test.rb
@@ -13,12 +13,14 @@ class CaseTest < ActiveSupport::TestCase
   end
 
   test "should not save case without a year" do
-    test_case = Case.new
-    assert_not test_case.save, "Saved case without a year"
+    test_case = Case.new(title: "What a day", abstract: "Why me?")
+    refute test_case.valid?, "Case is not valid without a year"
+    assert_not_nil test_case.errors[:year], "no validation error for year present"
   end
 
-  test "should not save case without a subject" do
-    test_case = Case.new
-    assert_not test_case.save, "Saved case without a subject"
+  test "should not save case without a abstract" do
+    test_case = Case.new(title: "What a day", year: 1984)
+    refute test_case.valid?, "Case is not valid without an abstarct"
+    assert_not_nil test_case.errors[:year], "no validation error for abstract present"
   end
 end
