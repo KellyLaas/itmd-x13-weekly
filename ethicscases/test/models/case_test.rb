edcf51d5e8bb392388db0724359f176239afdaff
KellyLaas <laas@iit.edu>
2017-06-13 15:55:59 -0500

    Edit fixtures to take out author and subject

--- ethicscases/test/models/case_test.rb
+++ ethicscases/test/models/case_test.rb
@@ -3,25 +3,13 @@ require 'test_helper'
 class CaseTest < ActiveSupport::TestCase
   def new_case
     Case.new(title: "My Case Title",
-              year: "1984",
-              abstract: "Bad stuff happens",
-    end
+              year: "yyyy",
+              abstract: "yyyy")
+  end
 
-  test "should have a title" do
+  test "should have a title, year and abstract" do
     test_case = Case.new
     assert test_case.invalid?
     assert test_case.errors[:title].any?
-  end
-
-  test "should have a abstract" do
-    test_case = Case.new
-    assert test_case.invalid?
-    assert test_case.errors[:abstract].any?
-  end
-
-  test "should have a year" do
-    test_case = Case.new
-    assert test_case.invalid?
-    assert test_case.errors[:year].any?
   end
 end
