72d2e2f80a9c110595a507ddd009042439ba99ce
KellyLaas <laas@iit.edu>
2017-06-20 10:39:09 -0500

    Fix extra end in line_item model

--- depot/app/models/line_item.rb
+++ depot/app/models/line_item.rb
@@ -2,7 +2,6 @@ class LineItem < ApplicationRecord
   belongs_to :order, optional: true
   belongs_to :product, optional: true
   belongs_to :cart
-end
 
   def total_price
     product.price * quantity
