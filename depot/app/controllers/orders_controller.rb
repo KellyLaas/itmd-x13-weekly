b9397657ba2701076da8d40c83d1c2d292e5ac90
KellyLaas <laas@iit.edu>
2017-06-20 12:30:24 -0500

    Fix spacing errors in line_item_controller

--- depot/app/controllers/orders_controller.rb
+++ depot/app/controllers/orders_controller.rb
@@ -79,11 +79,12 @@ class OrdersController < ApplicationController
     def order_params
       params.require(:order).permit(:name, :address, :email, :pay_type)
     end
+  #...
 
-    private
-      def ensure_cart_isnt_empty
-        if @cart.line_items.empty?
-          redirect_to store_index_url, notice: 'Your cart is empty'
+  private
+    def ensure_cart_isnt_empty
+      if @cart.line_items.empty?
+        redirect_to store_index_url, notice: 'Your cart is empty'
         end
       end
 end
