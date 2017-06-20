c3556b6f6aa67fd3fae4c6c845063feb2a1e1518
KellyLaas <laas@iit.edu>
2017-06-20 12:36:26 -0500

    Fix spacing errors in products_controller

--- depot/app/controllers/products_controller.rb
+++ depot/app/controllers/products_controller.rb
@@ -51,14 +51,14 @@ class ProductsController < ApplicationController
 
         @products = Product.all
         ActionCable.server.broadcast 'products',
-        html: render_to_string('store/index', layout: false)
-    else
-      format.html { render :edit }
-      format.json { render json: @product.errors,
-        status: :unprocessable_entity }
+          html: render_to_string('store/index', layout: false)
+      else
+        format.html { render :edit }
+        format.json { render json: @product.errors,
+          status: :unprocessable_entity }
+      end
     end
   end
-end
 
   # DELETE /products/1
   # DELETE /products/1.json
