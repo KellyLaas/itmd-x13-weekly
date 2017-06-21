3a927e45e5160fcac849ed37a89977c43a024c6b
KellyLaas <laas@iit.edu>
2017-06-21 16:11:14 -0500

    Create user model
      $rails g model User uid name email image_url --force

--- depot/app/models/user.rb
+++ depot/app/models/user.rb
@@ -1,4 +1,2 @@
 class User < ApplicationRecord
-  validates :name, presence: true, uniqueness: true
-  has_secure_password
 end
--- depot/db/migrate/20170621211010_create_users.rb
+++ depot/db/migrate/20170621211010_create_users.rb
@@ -1,8 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
+      t.string :uid
       t.string :name
-      t.string :password_digest
+      t.string :email
+      t.string :image_url
 
       t.timestamps
     end
