0c69d6fb0b02f2d7832fa49c2d4c25da061dfa9f
KellyLaas <laas@iit.edu>
2017-06-22 15:18:44 -0500

    Find or commit user from hash

--- depot/app/controllers/sessions_controller.rb
+++ depot/app/controllers/sessions_controller.rb
@@ -2,6 +2,11 @@ class SessionsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    @provider_response= request.env['omniauth.auth']
+    @provider_response = request.env['omniauth.auth'].inspect
+      auth_hash = request.env['omniauth.auth']
+    @user = User.find_or_create_by(uid: auth_hash['uid']) do |user|
+      user.name = auth_hash['info']['name']
+      user.nickname = auth_hash['info']['nickname']
+    end
   end
 end
